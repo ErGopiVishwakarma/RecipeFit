ebf1d122e0a9d9d9fcc07dcdc97d25a05a243428
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-07-18 12:44:45 +0530

    Update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Box>
+      {NavOptions.map((item, ind) => (
+        <NavLink to={item.to} key={item.name + ind}>
+          {item.name}
+        </NavLink>
+      ))}
+    </Box>
+  );
 };
 
 export default Navbar;
+
+const NavOptions = [
+  { to: "/", name: "Home" },
+  { to: "/articles", name: "Articles" },
+  { to: "/recipes", name: "Recipes" },
+];
