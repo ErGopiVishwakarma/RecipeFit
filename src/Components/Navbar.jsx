922e0817577a7fee2b64cc41f7e3ac8f60430e7c
Gopi vishwakarma <g2468vi@gmail.com>
2023-07-18 16:30:56 +0530

    initiial

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,40 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Image, Heading } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
+import { EmailIcon, Search2Icon } from "@chakra-ui/icons";
+import icon from '../Images/Icons/favicon.png'
 
 const Navbar = () => {
   return (
-    <Box>
-      {NavOptions.map((item, ind) => (
-        <NavLink to={item.to} key={item.name + ind}>
-          {item.name}
-        </NavLink>
-      ))}
+    <Box w='100%'>
+      <Flex justifyContent='space-evenly' h='80px' alignItems='center' bg='white'>
+        <Flex gap='30px'>
+          <Box>
+            <Search2Icon />
+          </Box>
+          <Flex gap='30px' fontSize='20px'>
+            <NavLink>Recipes</NavLink>
+            <NavLink>Articles</NavLink>
+            <NavLink>Videos</NavLink>
+          </Flex>
+        </Flex>
+        <Flex>
+          {/* <Image src={icon} w='100px' /> */}
+          <Heading>RecipeSnap</Heading>
+        </Flex>
+        <Flex gap='30px'>
+          <Flex gap='30px' fontSize='20px'>
+            <NavLink>App</NavLink>
+            <NavLink>Shop</NavLink>
+            <NavLink>About</NavLink>
+          </Flex>
+          <Box>
+            <EmailIcon />
+          </Box>
+        </Flex>
+      </Flex>
     </Box>
   );
 };
 
 export default Navbar;
-
-const NavOptions = [
-  { to: "/", name: "Home" },
-  { to: "/articles", name: "Articles" },
-  { to: "/recipes", name: "Recipes" },
-];
