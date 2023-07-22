98ebd2cb167ab660358c0050bc33e3316e338c02
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-07-22 18:27:26 +0530

    Update

--- src/Pages/SingleRecipePage.jsx
+++ src/Pages/SingleRecipePage.jsx
@@ -1,25 +1,42 @@
 import * as css from "../Styles/SingleRecipePageCss";
+import * as c from "../Styles/SuggestedAndCommentCss";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Input,
+  Button,
+  Editable,
+  EditableInput,
+  EditableTextarea,
+  EditablePreview,
+} from "@chakra-ui/react";
 
 import TempRecipeData from "../Functions/TempData";
 import SingleRecipeDetails from "../Components/RecipesPage/SingleRecipeDetails";
 import IngAndDirect from "../Components/RecipesPage/IngAndDirect";
+import SuggestedRecipes from "../Components/RecipesPage/SuggestedRecipes";
 
 const SingleRecipePage = () => {
   const { recipeID } = useParams();
   const [recipes, setRecipes] = useState(TempRecipeData);
   const [recipeData, setRecipeData] = useState({});
-
-  useEffect(() => {
-    console.log(recipeData);
-    document.title = `${recipeData.title} | RecipeSnap`;
-  }, [recipeData]);
+  const [suggested, setSuggested] = useState([]);
 
   useEffect(() => {
     filterRecipe();
   }, []);
+
+  useEffect(() => {
+    document.title = `${recipeData.title} | RecipeSnap`;
+    let ar = [];
+    for (let a = 0; a < 10; a++) {
+      suggested.push(recipeData.images);
+    }
+    setSuggested((prev) => ar);
+    //console.log(suggested);
+  }, [recipeData]);
 
   function filterRecipe() {
     let found = recipes?.filter((item, ind) => item._id == recipeID);
@@ -28,7 +45,6 @@ const SingleRecipePage = () => {
 
   return (
     <Box
-      marginBottom={"200px"}
       paddingBottom={["65px", "80px", "90px"]}
       paddingTop={["95px", "110px", "125px"]}
       //paddingTop={["65px", "75px", "85px"]}
@@ -38,6 +54,56 @@ const SingleRecipePage = () => {
 
       {/* Ingredients and Direction and Images */}
       <IngAndDirect recipeData={recipeData} />
+
+      {/* Suggested Recipes */}
+      {/* <SuggestedRecipes recipeData={recipeData} suggested={suggested} /> */}
+
+      {/* Discussion & Rating */}
+      <Box css={c.CommentOuter}>
+        <form>
+          <Text css={c.CommentHeader} fontFamily="cotorisb">
+            Discussion & Rating
+          </Text>
+          <textarea
+            className="commentTextArea"
+            type="text"
+            placeholder="Leave your comment here!"
+          />
+          <Box css={c.InputCont}>
+            <Input
+              _focusVisible="false"
+              color="dgreen"
+              borderColor="lblack2"
+              css={c.InputCss}
+              _placeholder={{ fontFamily: "cotorisb" }}
+              type="text"
+              placeholder="Name (shown with your comment)"
+            />
+            <Input
+              css={c.InputCss}
+              _focusVisible="false"
+              color="dgreen"
+              borderColor="lblack2"
+              _placeholder={{ fontFamily: "cotorisb" }}
+              type="email"
+              placeholder="Email (used only to notify you of replies)"
+            />
+          </Box>
+          <Button
+            type="submit"
+            isLoading={false}
+            css={c.SubmitBtn}
+            // spinner={<h1>xfvg</h1>}
+            loadingText="Submitting"
+            spinnerPlacement="end"
+            bg="greenbtn"
+            fontFamily="hind"
+            _hover={{ bg: "greenbtnhover" }}
+          >
+            Submit
+          </Button>
+        </form>
+      </Box>
     </Box>
   );
 };
