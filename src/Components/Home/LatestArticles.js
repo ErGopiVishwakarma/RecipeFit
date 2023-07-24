d5ec65fc411b5687d173525de0f5d1387b757dd5
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-07-24 11:15:15 +0530

    Removed Redux

--- src/Components/Home/LatestArticles.js
+++ src/Components/Home/LatestArticles.js
@@ -1,70 +1,89 @@
-
-import { Box, Button, Flex, Grid, Heading, Image, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import * as css from "../../Styles/AllRecipesBoxCss";
 import * as cs from "../../Styles/FilterRecipeStyles";
 
-import React, { useEffect, useState } from 'react'
-import { latestArticles } from './Dummy'
-import ArticleCarousal from '../carousal/ArticleCarousal'
-import { Link, NavLink } from 'react-router-dom';
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import { getArticles } from '../../Redux/Articles/action';
+import React, { useEffect, useState } from "react";
+import { latestArticles } from "./Dummy";
+import ArticleCarousal from "../carousal/ArticleCarousal";
+import { Link, NavLink } from "react-router-dom";
+import axios from "axios";
+import { getArticles } from "../../Redux/Articles/action";
 
 const LatestArticles = () => {
   //  const [articles,setArticles] = useState([])
-  const dispatch = useDispatch()
-  const articles = useSelector(store => store.articleReducer.articles)
-console.log(articles)
+  const dispatch = useDispatch();
+  const articles = useSelector((store) => store.articleReducer.articles);
+  console.log(articles);
   useEffect(() => {
-    dispatch(getArticles())
-  }, [])
+    dispatch(getArticles());
+  }, []);
 
   return (
-    <Flex direction={'column'} alignItems={'start'} px='4%' w='100%'>
-      <Heading py='30px' color={'gray'} fontFamily="cotorisb">Latest aritcles</Heading>
-      <Grid gridTemplateColumns={['none', 'repeat(2,1fr)', 'repeat(2,1fr)', 'repeat(3,1fr)']} display={['none', 'grid', 'grid', 'grid']} w='100%' gap='30px'>
-        {
-          articles?.map((el, ind) => {
-            return (
-              <NavLink to={`articles/${el._id}`} className="RecipeCardOuter" key={ind}>
-                <Box css={css.RecipeCardImgBox}>
-                  <Image src={el.image} css={css.RecipeCardImg} />
-                </Box>
-                <Flex direction={'column'} gap='-10px'>
-                  <Text fontSize={'1.4rem'} fontFamily="cotorisb">
-                    {el.heading}
-                  </Text>
-                  <Text fontSize={'1.2rem'} fontFamily="cotorisb">
-                    {el.subheading}
-                  </Text>
-                </Flex>
-              </NavLink>
-
-            )
-          })
-        }
+    <Flex direction={"column"} alignItems={"start"} px="4%" w="100%">
+      <Heading py="30px" color={"gray"} fontFamily="cotorisb">
+        Latest aritcles
+      </Heading>
+      <Grid
+        gridTemplateColumns={[
+          "none",
+          "repeat(2,1fr)",
+          "repeat(2,1fr)",
+          "repeat(3,1fr)",
+        ]}
+        display={["none", "grid", "grid", "grid"]}
+        w="100%"
+        gap="30px"
+      >
+        {articles?.map((el, ind) => {
+          return (
+            <NavLink
+              to={`articles/${el._id}`}
+              className="RecipeCardOuter"
+              key={ind}
+            >
+              <Box css={css.RecipeCardImgBox}>
+                <Image src={el.image} css={css.RecipeCardImg} />
+              </Box>
+              <Flex direction={"column"} gap="-10px">
+                <Text fontSize={"1.4rem"} fontFamily="cotorisb">
+                  {el.heading}
+                </Text>
+                <Text fontSize={"1.2rem"} fontFamily="cotorisb">
+                  {el.subheading}
+                </Text>
+              </Flex>
+            </NavLink>
+          );
+        })}
       </Grid>
       <Button
         bg="red.700"
         fontFamily="hind"
-        _hover={{ bg: "red.700", borderRadius: '50px' }}
+        _hover={{ bg: "red.700", borderRadius: "50px" }}
         isLoading={false}
-        m='auto'
-        display={['none', 'block']}
-        my='30px'
-        color={'white'}
-        w='auto'
+        m="auto"
+        display={["none", "block"]}
+        my="30px"
+        color={"white"}
+        w="auto"
       >
         more +
       </Button>
 
       {/* carousal here  */}
-      <Box display={['block', 'none']} w='100%' pos={'relative'}>
+      <Box display={["block", "none"]} w="100%" pos={"relative"}>
         <ArticleCarousal arr={articles} />
       </Box>
     </Flex>
-  )
-}
+  );
+};
 
 export default LatestArticles;
--- src/Components/Home/LatestRecipes.js
+++ src/Components/Home/LatestRecipes.js
@@ -1,60 +1,75 @@
-
-import { Box, Button, Flex, Grid, Heading, Image, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import * as css from "../../Styles/AllRecipesBoxCss";
-import React, { useEffect } from 'react'
-import { latestRecipes } from './Dummy.js'
-import RecipeCarousal from '../carousal/RecipeCarousal.js'
-import { Link, NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getRecipes } from '../../Redux/Articles/action';
+import React, { useEffect } from "react";
+import { latestRecipes } from "./Dummy.js";
+import RecipeCarousal from "../carousal/RecipeCarousal.js";
+import { Link, NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getRecipes } from "../../Redux/Articles/action";
 
 const LatestRecipes = () => {
-
-  const dispatch = useDispatch()
-  const recipes = useSelector(store => store.articleReducer.recipes)
-  console.log(recipes)
+  const dispatch = useDispatch();
+  const recipes = useSelector((store) => store.articleReducer.recipes);
+  console.log(recipes);
   useEffect(() => {
-    dispatch(getRecipes())
-  }, [])
+    dispatch(getRecipes());
+  }, []);
 
   return (
-    <Flex direction={'column'} alignItems={'start'} px='4%' w='100%'>
-      <Heading py='30px' color={'gray'} fontFamily="cotorisb">Latest Recipe</Heading>
-      <Grid gridTemplateColumns={['', 'repeat(2,1fr)', 'repeat(3,1fr)', 'repeat(4,1fr)']} display={['none', 'grid', 'grid', 'grid']} w='100%' gap='30px'>
-        {
-          recipes?.slice(0,8).map((el, ind) => {
-            return (
-              <NavLink to={`recipe/${el?._id}`} className="RecipeCardOuter" >
-                <Box css={css.RecipeCardImgBox}>
-                  <Image src={el?.img} css={css.RecipeCardImg} />
-                </Box>
-                <Text css={css.RecipeNameCss} fontFamily="cotorisb">
-                  {el?.title}
-                </Text>
-              </NavLink>
-
-            )
-          })
-        }
+    <Flex direction={"column"} alignItems={"start"} px="4%" w="100%">
+      <Heading py="30px" color={"gray"} fontFamily="cotorisb">
+        Latest Recipe
+      </Heading>
+      <Grid
+        gridTemplateColumns={[
+          "",
+          "repeat(2,1fr)",
+          "repeat(3,1fr)",
+          "repeat(4,1fr)",
+        ]}
+        display={["none", "grid", "grid", "grid"]}
+        w="100%"
+        gap="30px"
+      >
+        {recipes?.slice(0, 8).map((el, ind) => {
+          return (
+            <NavLink to={`recipe/${el?._id}`} className="RecipeCardOuter">
+              <Box css={css.RecipeCardImgBox}>
+                <Image src={el?.img} css={css.RecipeCardImg} />
+              </Box>
+              <Text css={css.RecipeNameCss} fontFamily="cotorisb">
+                {el?.title}
+              </Text>
+            </NavLink>
+          );
+        })}
       </Grid>
-      <Button 
+      <Button
         bg="red.700"
         fontFamily="hind"
-        _hover={{ bg: "red.700",borderRadius:'50px' }}
+        _hover={{ bg: "red.700", borderRadius: "50px" }}
         isLoading={false}
-        m='auto'
-        display={['none', 'block']}
-        my='30px'
-        color={'white'}
-        w='auto'
+        m="auto"
+        display={["none", "block"]}
+        my="30px"
+        color={"white"}
+        w="auto"
       >
         more +
       </Button>
       {/* carousal here  */}
-      <Box display={['block', 'none']} w='100%' pos={'relative'}>
+      <Box display={["block", "none"]} w="100%" pos={"relative"}>
         <RecipeCarousal arr={recipes} />
       </Box>
-    </Flex >
+    </Flex>
   );
 };
 
